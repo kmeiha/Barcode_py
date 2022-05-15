26a1eced71aeb4c57711826eaf1e1f0c1063b434
kmeiha <kmeihahassan@gmail.com>
2022-05-15 14:36:00 +0300

    database_fix

--- Barcode_Gui.py
+++ Barcode_Gui.py
@@ -1,6 +1,9 @@
+import re
 from tkinter import *
-from barcode import EAN13
 from tkinter import ttk
+from unittest import result
+from barcode import EAN13
+import barcode
 from barcode.writer import ImageWriter
 import os
 from tkinter import messagebox
@@ -15,32 +18,38 @@ root = Tk()
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 
-root.geometry("%dx%d" %(screen_width, screen_height))
+#root.geometry("%dx%d" %(screen_width, screen_height)) #Full screen
+root.geometry("630x630")
+root.resizable(False, False)
+root.title('Barcode System')
+root.iconbitmap('icon.ico')
 root.configure(bg="#15616D")
 
 checkboxva = IntVar()  # value of checkboxes
 current_var = StringVar()
 ID_Type = current_var.get()
 
-connection = sqlite3.connect("Fadl.db")
+connection = sqlite3.connect("nice.db")
   
 # cursor
-crsr = connection.cursor()
+cur = connection.cursor()
 
-sql_command = """CREATE TABLE IF NOT EXISTS main_table (id INT(3) Primary Key, name CHAR, dpt CHAR, type CHAR );"""
+sql_command = """CREATE TABLE IF NOT EXISTS main_table (
+                    id INT(3) Primary Key,
+                    name CHAR, 
+                    dpt CHAR,
+                    type CHAR );"""
 
-crsr.execute(sql_command)
+cur.execute(sql_command)
 
 def donothing():
-    x = 0
-
+    pass 
 
 def number_only(text):
     if str.isdigit(text):
         return True
     elif text == '':
         return True
-
     else:
         return False
 
@@ -48,10 +57,9 @@ def number_only(text):
 
     #tk = Toplevel(root, bg="#15616D")
 sql_select_Query = "select * from main_table"
+first =  cur.execute(sql_select_Query)
 
-crsr.execute(sql_select_Query)
-
-row_num = int(crsr.rowcount)
+row_num = int(cur.rowcount)
 
 main_list = []
 
@@ -60,7 +68,7 @@ Font_tuple = ("Comic Sans MS", 20, "bold",)
 Font_tuple2 = ("Comic Sans MS", 20, "bold",)
 
 choice = ['Pc', 'Laptop', 'Monitor', 'Keyboard',
-              'Mouse', 'Printer', 'Phone', 'Other']
+            'Mouse', 'Printer', 'Phone', 'Other']
 checkboxva = IntVar()
 
 var = checkboxva.get  # value of checkboxes
@@ -68,9 +76,9 @@ var = checkboxva.get  # value of checkboxes
 reg_fun = root.register(number_only)
 
 LabelName = Label(root, text="Name",
-                      bg="#15616D", fg="#EADEDA", font=Font_tuple).place(x=20, y=30)
+                    bg="#15616D", fg="#EADEDA", font=Font_tuple).place(x=20, y=30)
 EntryName = Entry(root, bg="#d8f3dc", width=20,
-                      validate='key')
+                    validate='key')
 EntryName.place(x=110, y=38)
 
 labelId = Label(root, text="Id",
@@ -80,12 +88,12 @@ EntryId = Entry(root, bg="#d8f3dc", width=20, validate='key',
 EntryId.place(x=105, y=137)
 
 labelType = Label(root, text="Type", bg="#15616D",
-                      fg="#EADEDA", font=Font_tuple).place(x=20, y=215)
+                    fg="#EADEDA", font=Font_tuple).place(x=20, y=215)
 ChoicesType = ttk.Combobox(
         root, values=choice, textvariable=current_var, state="readonly").place(x=105, y=225)
 
 LabelDpt = Label(root, text="Dpt.",
-                     bg="#15616D", fg="#EADEDA", font=Font_tuple).place(x=20, y=300)
+                    bg="#15616D", fg="#EADEDA", font=Font_tuple).place(x=20, y=300)
 EntryDpt = Entry(root, bg="#d8f3dc", width=16)
 EntryDpt.place(x=105, y=320)
 
@@ -121,46 +129,45 @@ def barecode():
     insert_data = "INSERT INTO main_table (id, name, dpt, type) VALUES (?, ?, ?, ?)"
     val = (ID, ID_name, ID_Dpt, ID_Type)
 
-    crsr.execute(insert_data, val)
+    cur.execute(insert_data, val)
+    global sql_con
+    sql_con = cur.execute("SELECT id FROM main_table")
+    global result
+    result = cur.fetchall()
     
-    crsr.execute("SELECT * FROM main_table")
-
-    result = crsr.fetchall()
+    
     print(result)
 
-    main_list.clear()
+    #main_list.clear()
 
     main_list.append(result)
-    
+    #print(main_list)
+    #################################################################
     ID = str(ID)
-    ID = ID + '000000000'
-    
-    with open('%s.txt' % ID_name, 'w', encoding='utf-8') as f:
+    ID = ID + '000000000'   #because it need 12 character
+    #################################################################
+    with open('backup_txt/%s.txt' % ID_name, 'w', encoding='utf-8') as f:
         f.write('ID : ' + ID + '\t')
         f.write('Name: ' + ID_name + '\n') 
         f.write('Department: ' + ID_Dpt + '\t')
         f.close()
-
-    my_code = EAN13(ID, writer=ImageWriter())
-    image_name = ID + '_' + ID_name
-    my_code.save(image_name)
+    #################################################################
+    my_code = EAN13(ID)
+    image_name =  ID + '_' + ID_name
+    my_code.save('Barcode_images/%s' %image_name)
+    #################################################################
 
     def image_opener():
         global image_name
         global checkboxva
         var = checkboxva.get()
-        if var == 1:  # If true open image
-            os.startfile('%s.png' % image_name)
-
+        if var == 1:  # If checked open image
+            os.startfile('Barcode_images/%s.svg' % image_name)
+    #################################################################
     image_opener()
     EntryName.delete(0, "end")
     EntryId.delete(0, "end")
     EntryDpt.delete(0, "end")
-    
-        
-
-        #IDname_Main = Label(frame, text="Type " + ID_Type)
-        # IDname_Main.place(x=10, y=10
 
 Create_btn = Button(root, text='Create', width=7, bg="#d8f3dc",
                         command=barecode).place(x=200, y=560)
@@ -168,7 +175,21 @@ Create_btn = Button(root, text='Create', width=7, bg="#d8f3dc",
 Cancel_btn = Button(root, text='Cancel', width=7, bg="#d8f3dc",
                         command=root.destroy).place(x=20, y=560)
 
-listbox = Listbox(root, width=50, height=46, listvariable=main_list).place(x=300, y=20)
+#################################################################
+sql_select_Query = "select id,name,type from main_table"
+first =  cur.execute(sql_select_Query)
+connection.commit()
+#################################################################
+def populatebox():
+    for i in first:
+        listBox.insert("end", i)
+#################################################################
+
+listBox = Listbox(root)
+listBox.pack()
+
+btn = Button(root, text="Update list", command = lambda: populatebox())
+btn.pack()
 menubar = Menu(root)
 
 filemenu = Menu(menubar, tearoff=0)
@@ -186,5 +207,7 @@ menubar.add_cascade(label="Help", menu=helpmenu)
 
 root.config(menu=menubar)
 
+
 root.mainloop()
 connection.commit()
+#connection.close()
